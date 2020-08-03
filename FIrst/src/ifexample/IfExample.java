2662d3890bbc88bcfb3678d971689224e3ffd459
Young-ju Na <young_7_4@naver.com>
2020-08-03 13:21:04 +0900

    20.08.03 desktop 조건문

---
+++ FIrst/src/ifexample/IfExample.java
@@ -0,0 +1,31 @@
+package ifexample;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int score = 101;
+		char grade;
+		
+		if(score > 100) {
+			System.out.println("전산오류입니다.");
+			return;
+		}
+		else if(score >= 90) {
+			grade = 'A';
+		} 
+		else if(score > 80){
+			grade = 'B';
+		}
+		else if(score > 70){
+			grade = 'C';
+		}
+		else if(score > 60) {
+			grade = 'D';
+		}
+		else {
+			grade = 'F';
+		}
+		System.out.println("성적은 " + grade + " 입니다.");
+	}
+
+}
--- FIrst/src/operator/OperationEx5.java
+++ FIrst/src/operator/OperationEx5.java
@@ -3,7 +3,11 @@ package operator;
 public class OperationEx5 {
 
 	public static void main(String[] args) {
+		int num = 0B00000101;	/* 5를 8비트 2진수로 나타냄 */
 		
+		System.out.println(num << 2);  // 왼쪽으로   2비트 이동 00010100 (20)
+		System.out.println(num >> 2);  // 오른쪽으로 2비트 이동 00000001 (1)
+		System.out.println(num >>> 2); // 오른쪽으로 2비트 이동 00000001 (1)
 	}
 
 }
